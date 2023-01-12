3eea3c61a44c305dc21c59728279d85853f03363
whoami2me <ldonen90@gmail.com>
2023-01-12 13:52:39 +0800

    new classes

--- OfflineEvents.py
+++ OfflineEvents.py
@@ -5,6 +5,8 @@ class OfflineEvents(Events.Events):
     count = 0
     def __init__(self, name, description, date, pax, location):
         super().__init__(name,description,date)
+        OfflineEvents.count += 1
+        self.__offline_event_id = OfflineEvents.count
         self.__pax = pax
         self.__location = location
 
@@ -19,4 +21,7 @@ class OfflineEvents(Events.Events):
 
     def get_location(self):
         return self.__location
+
+    def get_offline_event_id(self):
+        return self.__offline_event_id
 
--- OnlineEvents.py
+++ OnlineEvents.py
@@ -5,8 +5,12 @@ class OnlineEvents(Events.Events):
     count = 0
     def __init__(self, name, description, date):
         super().__init__(name,description,date)
+        OnlineEvents.count += 1
+        self.__online_event_id = OnlineEvents.count
 
 
+    def get_online_event_id(self):
+        return self.__online_event_id
 
 
 
