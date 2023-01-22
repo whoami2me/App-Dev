84271e2eae88a44707bf633a9418d415436f2772
whoami2me <ldonen90@gmail.com>
2023-01-22 15:19:06 +0800

    error404

--- SimpleWebApplication/__init__.py
+++ SimpleWebApplication/__init__.py
@@ -189,6 +189,10 @@ def update_offline(id):
 def get_map():
     return render_template('map.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error404.html'), 404
+
 if __name__ == '__main__':
     app.run()
 
