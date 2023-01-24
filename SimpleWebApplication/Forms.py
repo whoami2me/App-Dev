063070137b5b35aad64cb4af70ddce3b5924341e
whoami2me <ldonen90@gmail.com>
2023-01-25 01:00:23 +0800

    update validators (regex)

--- SimpleWebApplication/Forms.py
+++ SimpleWebApplication/Forms.py
@@ -1,20 +1,19 @@
 from wtforms import Form, StringField, TextAreaField, validators, IntegerField, SelectField
 from flask_wtf.file import FileField, FileAllowed, FileSize
 from wtforms.fields import DateField
-from wtforms_validators import AlphaNumeric
 
 
 class CreateEventForm(Form):
-    name = StringField('Name', [validators.Length(min=1, max=150), validators.DataRequired()])
-    image = FileField('Image', validators=[validators.DataRequired(), FileAllowed(['jpg', 'png', 'jpeg', 'gif'])])
+    name = StringField('Name', [validators.Length(min=1, max=150), validators.DataRequired(), validators.Regexp('[a-zA-Z0-9_-]+$', message="It should only contain letters, numbers , underscores and dashes")])
+    image = FileField('Image', validators=[validators.DataRequired(), FileAllowed(['jpg', 'png', 'jpeg', 'gif']), FileSize(max_size=16, min_size=2, message="Invalid file size")])
     description = TextAreaField('Description', [validators.Length(min=1, max=150), validators.DataRequired()])
     date = DateField('Date', validators=[validators.DataRequired()], format='%Y-%m-%d')
     location = SelectField('Location', validators=[validators.DataRequired()], choices=[('Z', 'Zoom'), ('G', 'Google Meets'), ('C', 'Cisco Webex')], default='')
 
 
 class CreateOEventForm(Form):
-    name = StringField('Name', [validators.Length(min=1, max=150), validators.DataRequired()])
-    image = FileField('Image', validators=[validators.DataRequired(), FileAllowed(['jpg', 'png', 'jpeg' ,'gif'])])
+    name = StringField('Name', [validators.Length(min=1, max=150), validators.DataRequired()], validators.Regexp('[a-zA-Z0-9_-]+$', message="It should only contain letters, numbers , underscores and dashes"))
+    image = FileField('Image', validators=[validators.DataRequired(), FileAllowed(['jpg', 'png', 'jpeg' ,'gif']), FileSize(max_size=16, min_size=2, message="Invalid file size")])
     description = TextAreaField('Description', [validators.Length(min=1, max=150), validators.DataRequired()])
     date = DateField('Date', validators=[validators.DataRequired()], format='%Y-%m-%d')
     location = SelectField('Location', validators=[validators.DataRequired()], choices=[('Z', 'Zoom'), ('G', 'Google Meets'), ('C', 'Cisco Webex')], default='')
@@ -23,8 +22,8 @@ class CreateOEventForm(Form):
 
 
 class CreateOfflineEventForm(Form):
-    name = StringField('Name', [validators.Length(min=1, max=150), validators.DataRequired()])
-    image = FileField('Image', validators=[validators.DataRequired(), FileAllowed(['jpg', 'png', 'jpeg' ,'gif'])])
+    name = StringField('Name', [validators.Length(min=1, max=150), validators.DataRequired(), validators.Regexp('[a-zA-Z0-9_-]+$', message="It should only contain letters, numbers , underscores and dashes")])
+    image = FileField('Image', validators=[validators.DataRequired(), FileAllowed(['jpg', 'png', 'jpeg' ,'gif']), FileSize(max_size=16, min_size=2, message="Invalid file size")])
     description = TextAreaField('Description', [validators.Length(min=1, max=150), validators.DataRequired()])
     date = DateField('Date', validators=[validators.DataRequired()], format='%Y-%m-%d')
     pax = IntegerField('Pax', [validators.NumberRange(min=0, max=1000), validators.DataRequired()])
@@ -32,8 +31,8 @@ class CreateOfflineEventForm(Form):
 
 
 class CreateOffEventForm(Form):
-    name = StringField('Name', [validators.Length(min=1, max=150), validators.DataRequired()])
-    image = FileField('Image', validators=[validators.DataRequired(), FileAllowed(['jpg', 'png', 'jpeg' ,'gif'])])
+    name = StringField('Name', [validators.Length(min=1, max=150), validators.DataRequired(), validators.Regexp('[a-zA-Z0-9_-]+$', message="It should only contain letters, numbers , underscores and dashes")])
+    image = FileField('Image', validators=[validators.DataRequired(), FileAllowed(['jpg', 'png', 'jpeg' ,'gif']), FileSize(max_size=16, min_size=2, message="Invalid file size")])
     description = TextAreaField('Description', [validators.Length(min=1, max=150), validators.DataRequired()])
     date = DateField('Date', validators=[validators.DataRequired()], format='%Y-%m-%d')
     pax = IntegerField('Pax', [validators.NumberRange(min=0, max=1000), validators.DataRequired()])
