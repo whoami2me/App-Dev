8d5bade41543a2957245a756f643d0b5d223afdd
Rayden <raydenleau@gmail.com>
2023-01-25 14:51:00 +0800

    Added validation for fileupload

--- ProductForms.py
+++ ProductForms.py
@@ -1,9 +1,9 @@
 from wtforms import Form, StringField, FileField, SelectMultipleField, TextAreaField, DecimalField,IntegerField,validators
- 
+from flask_wtf.file import FileAllowed
 class CreateProduct(Form):
     name = StringField('Name: ', [validators.DataRequired()])
     desc = TextAreaField('Description: ', [validators.Optional()])
     price = DecimalField('Price: ', [validators.DataRequired()])
     qty = IntegerField('Quantity: ', [validators.DataRequired()])
     grp = SelectMultipleField("Category: ",[validators.DataRequired()], choices=[('Shoes','Shoes'),('Shirts','Shirts',),('Pants','Pants'),('Accessories','Accessories')])
-    image = FileField('Image: ',[validators.DataRequired()])
+    image = FileField('Image: ',validators=[[validators.DataRequired()], FileAllowed(['jpg', 'png', 'jpeg', 'gif'])])
--- macvenv/bin/activate.csh
+++ macvenv/bin/activate.csh
@@ -8,7 +8,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV "/Users/rayden/Documents/School Code/AppDev/Project/macvenv"
+setenv VIRTUAL_ENV "/Users/rayden/Documents/School Code/AppDev/untitled folder/App-Dev/macvenv"
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/bin:$PATH"
