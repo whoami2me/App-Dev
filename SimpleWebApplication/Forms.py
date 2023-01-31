90a8627a0cd4bf1b61527bd08bff3b3a95cc22d0
whoami2me <ldonen90@gmail.com>
2023-01-31 22:01:10 +0800

    Integration test 2 (Success) (Incomplete - Inventory)

--- SimpleWebApplication/Forms.py
+++ SimpleWebApplication/Forms.py
@@ -161,5 +161,5 @@ class CreateSuppliersForm(Form):
 class CreateInventoryForm(Form):
     Categories_select = SelectField('Categories', [validators.DataRequired()], choices=[('', 'Select'), ('B', 'Ball'), ('F', 'Footwear')],default='')
     Product_name = StringField('Name of product', [validators.Length(min=1, max=150), validators.DataRequired()])
-    Qty = IntegerField('Quantity: ',[validators.NumberRange(min=1,max=100),validators.DataRequired])
+    Qty = IntegerField('Quantity: ',[validators.NumberRange(min=1,max=100),validators.DataRequired()])
     remarks = TextAreaField('Remarks', [validators.Optional()])
