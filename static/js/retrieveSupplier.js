156daaa86f983341c9ab22dcf9b510b7df44cd5a
Gamma7787 <muhammadizwan814@gmail.com>
2023-02-17 17:58:42 +0800

    change icon

--- static/js/retrieveSupplier.js
+++
@@ -1,6 +0,0 @@
-  var button = document.getElementById('order');
-  if (document.getElementsByClassName('Type2').length > 0) {
-    button.disabled = true;
-  } else {
-    button.disabled = false;
-  }
